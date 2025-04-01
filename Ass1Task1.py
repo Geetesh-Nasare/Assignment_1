3ca755374fda4db07c8cf7a2a2972d2129ac8d06
Geetesh Nasare <155899505+Geetesh-Nasare@users.noreply.github.com>
2025-04-01 16:08:24 +0530

    Add files via upload

---
+++ Ass1Task1.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+Add = num1 + num2
+print("Addition: ",Add)
+
+Sub = num1 - num2
+print("Subtraction: ",Sub)
+
+Multi = num1 * num2
+print("Multiplication: ",Multi)
+
+Div = num1 / num2
+print("Division: ",Div)
---
+++ Ass1Task2.py
@@ -0,0 +1,4 @@
+name = str(input("Enter your first name: "))
+sur = str(input("Enter your last name: "))
+
+print("Hello,",name,sur,"! Welcome to the Python Program." )
